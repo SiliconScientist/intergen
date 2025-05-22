82c3c098a0ddebd3b4537f0ee9712ff8f2640ff9
Avery <ahill15@tulane.edu>
2025-05-22 12:14:02 -0500

    Achieve god-tier symmetry filtering for ontop sites

--- src/intergen/__main__.py
+++ src/intergen/__main__.py
@@ -7,6 +7,10 @@ from intergen.surface import (
     build_pure_surfaces,
     naive_surface_index_selector,
     mutate_via_swaps,
+    get_atoms_per_layer,
+    find_unique_structures,
+    get_substructure,
+    prepare_for_pymatgen,
 )
 
 
@@ -26,29 +30,34 @@ def main():
         pure_atoms=pure_atoms,
     )
     print(f"Generated {len(atoms_list)} unique structures.")
-    # for atoms in atoms_list:
-    #     slab = converter.get_structure(atoms)
-    #     site_finder = AdsorbateSiteFinder(slab=slab)
-    #     site_coordinates = site_finder.find_adsorption_sites(symm_reduce=True)
-    #     adsorbate = Molecule(
-    #         species=cfg.adsorbate.species,
-    #         coords=cfg.adsorbate.coords,
-    #         site_properties={
-    #             "tags": [cfg.adsorbate.tag for _ in range(len(cfg.adsorbate.coords))]
-    #         },
-    #     )
-    #     surface_indices = range(get_atoms_per_layer(cfg=cfg))
-    #     structures = []
-    #     for site in site_coordinates["ontop"]:
-    #         structure = site_finder.add_adsorbate(
-    #             molecule=adsorbate, ads_coord=site_coordinates["ontop"][0]
-    #         )
-    #     adsorbate_indices = [i for i in range(len(structure))][-2:]
-    #     comparison_indices = surface_indices + adsorbate_indices
-    #     structure = find_unique_structures(
-    #         atoms_list=structure, comparison_indices=comparison_indices
-    #     )
-    #     atoms = converter.get_atoms(structure)
+    slabs = prepare_for_pymatgen(atoms_list)
+    all_binding_sites = []
+    for slab in slabs:
+        site_finder = AdsorbateSiteFinder(slab=slab)
+        site_coordinates = site_finder.find_adsorption_sites(symm_reduce=False)
+        adsorbate = Molecule(
+            species=cfg.adsorbate.species,
+            coords=cfg.adsorbate.coords,
+            site_properties={
+                "tags": [cfg.adsorbate.tag for _ in range(len(cfg.adsorbate.coords))]
+            },
+        )
+        structures = []
+        for site in site_coordinates["ontop"]:
+            structure = site_finder.add_adsorbate(molecule=adsorbate, ads_coord=site)
+            structures.append(structure)
+        adsorbate_indices = [i for i in range(len(structure))][-2:]
+        surface_indices = list(range(get_atoms_per_layer(cfg=cfg)))
+        comparison_indices = surface_indices + adsorbate_indices
+        subsubstructures = [
+            get_substructure(structure, indices=comparison_indices)
+            for structure in structures
+        ]
+        unique_indices = find_unique_structures(structures=subsubstructures)
+        unique_structures = [structures[i] for i in unique_indices]
+        unique_atoms = [converter.get_atoms(struct) for struct in unique_structures]
+        all_binding_sites.extend(unique_atoms)
+    print(f"Generated {len(all_binding_sites)} unique binding sites.")
 
 
 if __name__ == "__main__":
