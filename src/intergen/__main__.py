c89fe9130e73ff732c37d104c48ae8ad2977f84f
Avery <ahill15@tulane.edu>
2025-05-22 15:25:48 -0500

    Move adsorption site generation logic to adsorbate.py module

--- src/intergen/__main__.py
+++ src/intergen/__main__.py
@@ -8,11 +8,8 @@ from intergen.surface import (
     build_pure_surfaces,
     naive_surface_index_selector,
     mutate_via_swaps,
-    get_atoms_per_layer,
-    find_unique_structures,
-    get_substructure,
-    prepare_for_pymatgen,
 )
+from intergen.adsorbate import get_adsorbate_structures
 
 
 def main():
@@ -33,39 +30,17 @@ def main():
         matcher=matcher,
     )
     print(f"Generated {len(atoms_list)} unique structures.")
-    slabs = prepare_for_pymatgen(atoms_list)
-    all_binding_sites = []
-    for slab in slabs:
-        site_finder = AdsorbateSiteFinder(slab=slab)
-        site_coordinates = site_finder.find_adsorption_sites(symm_reduce=False)
-        adsorbate = Molecule(
-            species=cfg.adsorbate.species,
-            coords=cfg.adsorbate.coords,
-            site_properties={
-                "tags": [cfg.adsorbate.tag for _ in range(len(cfg.adsorbate.coords))]
-            },
-        )
-        structures = []
-        for site in cfg.adsorbate.sites:
-            for ads_coords in site_coordinates[site]:
-                structure = site_finder.add_adsorbate(
-                    molecule=adsorbate, ads_coord=ads_coords
-                )
-                structures.append(structure)
-        adsorbate_indices = [i for i in range(len(structure))][-2:]
-        surface_indices = list(range(get_atoms_per_layer(cfg=cfg)))
-        comparison_indices = surface_indices + adsorbate_indices
-        subsubstructures = [
-            get_substructure(structure, indices=comparison_indices)
-            for structure in structures
-        ]
-        unique_indices = find_unique_structures(
-            structures=subsubstructures, matcher=matcher
-        )
-        unique_structures = [structures[i] for i in unique_indices]
-        unique_atoms = [converter.get_atoms(struct) for struct in unique_structures]
-        all_binding_sites.extend(unique_atoms)
-    print(f"Generated {len(all_binding_sites)} unique binding sites.")
+    adsorbate = Molecule(
+        species=cfg.adsorbate.species,
+        coords=cfg.adsorbate.coords,
+        site_properties={
+            "tags": [cfg.adsorbate.tag for _ in range(len(cfg.adsorbate.coords))]
+        },
+    )
+    atoms_list = get_adsorbate_structures(
+        cfg=cfg, atoms_list=atoms_list, adsorbate=adsorbate, matcher=matcher
+    )
+    print(f"Generated {len(atoms_list)} unique binding sites.")
 
 
 if __name__ == "__main__":
---
+++ src/intergen/adsorbate.py
@@ -0,0 +1,61 @@
+from ase import Atoms
+from pymatgen.core import Molecule, Structure
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen.analysis.adsorption import AdsorbateSiteFinder
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from intergen.config import Config
+from intergen.surface import (
+    prepare_for_pymatgen,
+    get_atoms_per_layer,
+    find_unique_structures,
+    get_substructure,
+)
+
+
+def add_adsorbates(cfg: Config, structure, adsorbate: Molecule) -> list[Structure]:
+    site_finder = AdsorbateSiteFinder(slab=structure)
+    site_coordinates = site_finder.find_adsorption_sites(symm_reduce=False)
+    structures = []
+    for site in cfg.adsorbate.sites:
+        for ads_coords in site_coordinates[site]:
+            structure = site_finder.add_adsorbate(
+                molecule=adsorbate, ads_coord=ads_coords
+            )
+            structures.append(structure)
+    return structures
+
+
+def get_adsorbate_indices(structure: Structure, adsorbate: Molecule) -> list[int]:
+    """Returns the indices of the adsorbate in the structure."""
+    structure_len = len(structure)
+    adsorbate_len = len(adsorbate)
+    adsorbate_indices = list(range(structure_len, structure_len + adsorbate_len))
+    return adsorbate_indices
+
+
+def get_adsorbate_structures(
+    cfg: Config,
+    atoms_list: list[Atoms],
+    adsorbate: Molecule,
+    matcher: StructureMatcher,
+    converter: AseAtomsAdaptor = AseAtomsAdaptor(),
+):
+    """Generates all unique structures with the specified adsorbate."""
+    slabs = prepare_for_pymatgen(atoms_list)
+    atoms_list = []
+    for slab in slabs:
+        structures = add_adsorbates(cfg=cfg, structure=slab, adsorbate=adsorbate)
+        adsorbate_indices = get_adsorbate_indices(structure=slab, adsorbate=adsorbate)
+        surface_indices = list(range(get_atoms_per_layer(cfg=cfg)))
+        comparison_indices = surface_indices + adsorbate_indices
+        subsubstructures = [
+            get_substructure(structure, indices=comparison_indices)
+            for structure in structures
+        ]
+        unique_indices = find_unique_structures(
+            structures=subsubstructures, matcher=matcher
+        )
+        unique_structures = [structures[i] for i in unique_indices]
+        unique_atoms = [converter.get_atoms(struct) for struct in unique_structures]
+        atoms_list.extend(unique_atoms)
+    return atoms_list
